2ba8052060c9cc330b5a73da7048b25c5bf3be20
Vaibhav Bedarkar <vaibhav.bedarkar98@gmail.com>
2021-02-26 14:04:14 +0530

    Firebase Authentication Successful

--- app/src/main/java/com/vaibhavbedarkar/learn/LoginRegistration.java
+++ app/src/main/java/com/vaibhavbedarkar/learn/LoginRegistration.java
@@ -1,5 +1,6 @@
 package com.vaibhavbedarkar.learn;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -8,13 +9,20 @@ import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class LoginRegistration extends AppCompatActivity {
 
     Button forgot_password, button_registration, button_login;
+
+    EditText userEmail,userPassword;
+    String email,password;
 
     private FirebaseAuth mAuth;
 
@@ -25,35 +33,48 @@ public class LoginRegistration extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
         setContentView(R.layout.activity_login_registration);
 
-        mAuth = FirebaseAuth.getInstance();
-
-        FirebaseUser currentUser = mAuth.getCurrentUser();
-
-        EditText userEmail,userPassword ;
-        String email,password;
-
-
         userEmail = findViewById(R.id.login_email);
         userPassword = findViewById(R.id.login_password);
-
-        email = userEmail.getText().toString();
-        password = userPassword.getText().toString();
-
 
         forgot_password = findViewById(R.id.button_forgotpassword);
         button_login = findViewById(R.id.button_login);
         button_registration = findViewById(R.id.button_registration);
 
+
+
+        mAuth = FirebaseAuth.getInstance();
+
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser!=null){
+            Intent intent = new Intent(LoginRegistration.this, Dashboard.class);
+            startActivity(intent);
+        }
+
         button_login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                //method for firebase authentication
+                email = userEmail.getText().toString();
+                password = userPassword.getText().toString();
 
-                // firebase session
-                Intent intent = new Intent(LoginRegistration.this, Dashboard.class);
-                startActivity(intent);
-                finish();
+                mAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            FirebaseUser user = mAuth.getCurrentUser();
+
+                            Toast.makeText(LoginRegistration.this,"Welcome "+email,Toast.LENGTH_LONG).show();
+                            Intent in = new Intent(LoginRegistration.this,Dashboard.class);
+                            startActivity(in);
+                            finish();
+                        }
+                        else{
+
+                            Toast.makeText(LoginRegistration.this,"Auth Failed",Toast.LENGTH_SHORT).show();
+
+                        }
+                    }
+                });
             }
         });
 
@@ -68,6 +89,4 @@ public class LoginRegistration extends AppCompatActivity {
 
     }
 
-    private void updateUI() {
-    }
 }
